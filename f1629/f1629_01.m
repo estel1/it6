60d54921ae3ef779907324a21fcc10ce85752c4b
Melnikov Aleksey <melnikov.aleksey@gmail.com>
2010-06-29 15:24:42 +0400

    f1629 added

---
+++ f1629/f1629_01.m
@@ -0,0 +1,17 @@
+function f1629_01
+clc, clear all ;
+plot([0 1 1 2 2 3 4 5 5 6 6 7 7 8 9],...
+     [0 0 0 0 1 1 1 1 0 0 1 1 0 0 0], 'LineWidth', 2) ;
+ylim([-.5 1.5]) ;
+xlim([-.5 9.5]) ;
+grid on;
+text(0.5,0.1,'0','FontSize',12) ;
+text(1.5,0.1,'0','FontSize',12) ;
+text(2.5,1.1,'1','FontSize',12) ;
+text(3.5,1.1,'1','FontSize',12) ;
+text(4.5,1.1,'1','FontSize',12) ;
+text(5.5,0.1,'0','FontSize',12) ;
+text(6.5,1.1,'1','FontSize',12) ;
+text(7.5,0.1,'0','FontSize',12) ;
+text(8.5,0.1,'0','FontSize',12) ;
+ 
